f84caa5d967b663b583157cd05c4dbafab01e60d
Reinhard Herrmann <reinhard-ulrich.herrmann@web.de>
2022-12-10 23:22:49 +0100

    Code Changes:
    build.gradle
    RecipeListFragment.kt
    RecipeFragment.kt
    main_graph.xml
    activity_main
    MainActivity.kt

--- app/src/main/java/de/orome/mvvmrecipeapp/RecipeListFragment.kt
+++ app/src/main/java/de/orome/mvvmrecipeapp/RecipeListFragment.kt
@@ -9,15 +9,20 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class RecipeListFragment : Fragment() {
 
@@ -26,24 +31,25 @@ class RecipeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
-
-        view.findViewById<ComposeView>(R.id.compose_view).setContent {
-            Column(
-                modifier = Modifier
-                    .border(border = BorderStroke(1.dp, Color.Black))
-                    .padding(16.dp)
-            ) {
-                Text(text = "This is a composable inside the fragment")
-                Spacer(modifier = Modifier.padding(10.dp))
-                CircularProgressIndicator()
-                Text(text = "NEAT")
-                Spacer(modifier = Modifier.padding(10.dp))
-                val customView = HorizontalDottedProgress(LocalContext.current)
-                AndroidView(factory = {customView})
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "Recipe List",
+                        style =  MaterialTheme.typography.h5
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(
+                        onClick = {
+                            findNavController().navigate(R.id.action_view_recipe_detail)
+                        }
+                    ) {
+                        Text(text = "to Recipe details")
+                    }
+                }
             }
         }
-
-        return view
     }
 }
