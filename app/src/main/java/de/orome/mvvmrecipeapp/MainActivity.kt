2831d7d5dc0ea99a074201d47e9d85c4b02f7617
Reinhard Herrmann <reinhard-ulrich.herrmann@web.de>
2022-12-10 22:47:57 +0100

    Code Changes:
    RecipeListFragment.kt
    fragment_recipe_list.xml
    activity_main.xml
    MainActivity.kt
    HorizontalDottedProgress.kt

--- app/src/main/java/de/orome/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/de/orome/mvvmrecipeapp/MainActivity.kt
@@ -2,29 +2,14 @@ package de.orome.mvvmrecipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-            ) {
-Text(text = "Hey look, some text!")
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "A nice Button")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContainer, RecipeListFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/de/orome/mvvmrecipeapp/RecipeListFragment.kt
@@ -0,0 +1,49 @@
+package de.orome.mvvmrecipeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.fragment.app.Fragment
+
+class RecipeListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
+
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+            Column(
+                modifier = Modifier
+                    .border(border = BorderStroke(1.dp, Color.Black))
+                    .padding(16.dp)
+            ) {
+                Text(text = "This is a composable inside the fragment")
+                Spacer(modifier = Modifier.padding(10.dp))
+                CircularProgressIndicator()
+                Text(text = "NEAT")
+                Spacer(modifier = Modifier.padding(10.dp))
+                val customView = HorizontalDottedProgress(LocalContext.current)
+                AndroidView(factory = {customView})
+            }
+        }
+
+        return view
+    }
+}
